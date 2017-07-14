5573a830e37ac66de5d2463473cdbe69e30d6f76
yunkyuhee <ojinga0519@naver.com>
2017-07-14 12:15:26 +0900

    submit_button

--- components/about.js
+++ components/about.js
@@ -16,11 +16,12 @@ angular.module('hello')
       
       $scope.onclick = function onclick(){       
         console.log('scope:', $scope);
-          FormListService.add($scope.qna);
+          FormListService.add($scope.projectForm);
           //$scope.clientName = "";
         
         //$mdToast.show($mdToast.simple().textContent('문의가 완료되었습니다!'));
         //$location.path('/components/list.html');
+       
         $state.transitionTo("list");
       };
     }
