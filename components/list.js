23957ed669f6e64a09d5e638e32dc773122931cd
sun <sun@hummingtop.co.kr>
2017-07-17 17:57:05 +0900

    error

--- components/list.js
+++ components/list.js
@@ -4,7 +4,7 @@ angular.module('hello')
   .component('list', {
   templateUrl:  '/components/list.html',
    controller: function($scope, FormListService) {
-      $scope.codes = FormListService.get();      
+      $scope.codes = FormListService.get();    
     }
   }
 )
