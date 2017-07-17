480506d18cd227ea21924906d1d047e5b2fd85f8
sun <sun@hummingtop.co.kr>
2017-07-17 12:08:25 +0900

    except Phone

--- components/about.js
+++ components/about.js
@@ -16,12 +16,12 @@ angular.module('hello')
       
       $scope.onclick = function onclick(){       
         console.log('scope:', $scope);
-          FormListService.add($scope.projectForm);
+          FormListService.add($scope.qna);
           //$scope.clientName = "";
         
         //$mdToast.show($mdToast.simple().textContent('문의가 완료되었습니다!'));
         //$location.path('/components/list.html');
-        //$state.transitionTo("list");
+        $state.transitionTo("list");
       };
     }
   }
