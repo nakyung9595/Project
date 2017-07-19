a984ecc7a58e263d6cff4c06db9081ad50d4c912
yunkyuhee <ojinga0519@naver.com>
2017-07-19 11:40:03 +0900

    paging

--- components/list.js
+++ components/list.js
@@ -4,7 +4,26 @@ angular.module('hello')
   .component('list', {
   templateUrl:  '/components/list.html',
    controller: function($scope, FormListService) {
-      $scope.codes = FormListService.get();    
+      $scope.codes = FormListService.get();   
+
+      $scope.query = {
+      
+        order: 'name',
+        limit: 5,
+        page: 1
+  };
+
+  $scope.options = {
+    //autoSelect: true,
+    boundaryLinks: true,
+    //largeEditDialog: true,
+    pageSelector: true,
+    rowSelection: true
+  };
+  $scope.limitOptions = [5, 10, 15]; //How many entries per page options
+
+
+  
     }
   }
 )
